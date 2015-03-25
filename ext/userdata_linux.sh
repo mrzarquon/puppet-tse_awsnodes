44c1b60603134bc0d6ed0511340a6d33b43874ee
Chris Barker <cbarker@puppetlabs.com>
2015-03-25 13:54:20 -0700

    more cleaning up

--- ext/userdata_linux.sh
+++ ext/userdata_linux.sh
@@ -23,23 +23,22 @@ function write_csr_attributes () {
 extension_requests:
   pp_instance_id: $PP_INSTANCE_ID
   pp_image_name: $PP_IMAGE_NAME
+  pp_uuid: 'pp_uuid BD018C91-EEA2-4641-85C1-3EEA09282639'
+  pp_preshared_key: 'pp_preshared_key B7A7136A-5850-4315-BAB6-C3DFE4B41487'
+  1.3.6.1.4.1.34380.1.1.5: 'pp_cost_center tse'
+  1.3.6.1.4.1.34380.1.1.6: 'pp_product testing code'
+  1.3.6.1.4.1.34380.1.1.7: 'pp_project'
+  1.3.6.1.4.1.34380.1.1.8: 'pp_application'
+  1.3.6.1.4.1.34380.1.1.9: 'pp_service'
+  1.3.6.1.4.1.34380.1.1.10: 'pp_employee'
+  1.3.6.1.4.1.34380.1.1.11: 'pp_created_by'
+  1.3.6.1.4.1.34380.1.1.12: 'pp_environment' 
+  1.3.6.1.4.1.34380.1.1.13: 'pp_role'
+  1.3.6.1.4.1.34380.1.1.14: 'pp_software_version'
+  1.3.6.1.4.1.34380.1.1.15: 'pp_department' 
+  1.3.6.1.4.1.34380.1.1.16: 'pp_cluster'
+  1.3.6.1.4.1.34380.1.1.17: 'pp_provisioner'
 YAML
-  
-  echo '  pp_uuid: pp_uuid BD018C91-EEA2-4641-85C1-3EEA09282639' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
-  echo '  pp_preshared_key: pp_preshared_key B7A7136A-5850-4315-BAB6-C3DFE4B41487' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
-  echo '  1.3.6.1.4.1.34380.1.1.5: pp_cost_center tse' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
-  echo '  1.3.6.1.4.1.34380.1.1.6: pp_product testing code' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
-  echo '  1.3.6.1.4.1.34380.1.1.7: pp_project' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
-  echo '  1.3.6.1.4.1.34380.1.1.8: pp_application' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
-  echo '  1.3.6.1.4.1.34380.1.1.9: pp_service' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
-  echo '  1.3.6.1.4.1.34380.1.1.10: pp_employee' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
-  echo '  1.3.6.1.4.1.34380.1.1.11: pp_created_by' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
-  echo '  1.3.6.1.4.1.34380.1.1.12: pp_environment' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
-  echo '  1.3.6.1.4.1.34380.1.1.13: pp_role' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
-  echo '  1.3.6.1.4.1.34380.1.1.14: pp_software_version' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
-  echo '  1.3.6.1.4.1.34380.1.1.15: pp_department' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
-  echo '  1.3.6.1.4.1.34380.1.1.16: pp_cluster' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
-  echo '  1.3.6.1.4.1.34380.1.1.17: pp_provisioner' >> /etc/puppetlabs/puppet/csr_attributes.yaml 
 }
 
 function install_pe_agent () {
