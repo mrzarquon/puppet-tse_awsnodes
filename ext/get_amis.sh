6fceed4271e6fac25a73000b14c7a2109c0c0ef5
Chris Barker <cbarker@puppetlabs.com>
2015-04-08 11:26:30 -0700

    fixes windows 2008r2 image
    was pointing to 2008 ami and named incorrectly in params.pp

--- ext/get_amis.sh
+++ ext/get_amis.sh
@@ -13,7 +13,7 @@ for region in "${REGIONS[@]}"; do
   aws ec2 describe-images --owners 309956199498 --region $region --filters Name=name,Values=*6*,*7* Name=virtualization-type,Values=hvm Name=state,Values=available --query 'Images[*].{Name:Name,AMI:ImageId}' --output text | sort -k2 >> $region.txt
   sleep 5
   # windows
-  aws ec2 describe-images --owners 801119661308 --region $region --filters Name=name,Values=*2012*English*Base*,*2008*SP2*English*64Bit*Base* Name=virtualization-type,Values=hvm Name=state,Values=available --query 'Images[*].{Name:Name,AMI:ImageId}' --output text | sort -k2 >> $region.txt
+  aws ec2 describe-images --owners 801119661308 --region $region --filters Name=name,Values=*2012*English*Base*,*2008*R2*English*64Bit*Base* Name=virtualization-type,Values=hvm Name=state,Values=available --query 'Images[*].{Name:Name,AMI:ImageId}' --output text | sort -k2 >> $region.txt
   sleep 5
   # amazon
   aws ec2 describe-images --owners 137112412989 --region $region --filters Name=name,Values=*2015* Name=virtualization-type,Values=hvm Name=state,Values=available --query 'Images[*].{Name:Name,AMI:ImageId}' --output text | sort -k2 >> $region.txt
